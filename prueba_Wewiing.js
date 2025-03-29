1ed2655aaef3e9a8ad30d0d837d9b5daeca72f32
Wewiing <edwinosorio29092004@gmail.com>
2025-03-29 09:30:35 -0500

    Se hace fetch a la API rickandmorty

--- prueba_Wewiing.js
+++ prueba_Wewiing.js
@@ -1,1 +1,7 @@
 console.log("hello world!");
+
+fetch("https://rickandmortyapi.com/api/character").then((res) =>{
+    return res.json(); //transforma la respuesta y la convierte en un objeto
+}).then((data) =>{
+    console.log(data);
+})
